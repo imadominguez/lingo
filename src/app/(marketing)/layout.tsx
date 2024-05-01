735ef5367d67895484e4997b670794c6cd404c87
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-05-01 10:48:13 -0300

    Main Layout

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -4,11 +4,11 @@ import { Header } from "./header";
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col">
-      <Header/>
+      <Header />
       <main className="flex-1 flex flex-col items-center justify-center">
         {children}
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
